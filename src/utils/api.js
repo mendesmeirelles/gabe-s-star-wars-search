e43da842aaf9562f9466cf1356ed2da31d871acb
Gabriel Meirelles <mendesmeirelles@gmail.com>
2018-02-05 12:57:05 +0000

    Update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,6 @@
 import axios from "axios";
 
-/**
- * Recursively get the data from the given URL. 
- * Keep fetching the data as long as the returning results
- * contains a "next page". Parse the results into an array.
- *
- * @param {string} url This is the starting URL we need to fetch 
- * 		data from.
- * @return {Promise} This returns a promise that yields the
- *      (future) fetched array. 
- */
+
 export function fetchData(url) {
 	var arr = [];
 	
@@ -33,15 +24,7 @@ export function fetchData(url) {
 	})
 } 
 
-/**
- * Fetch data from the given URL. 
- * Parse the result into an object
- *
- * @param {string} url This is the URL we need to fetch 
- * 		data from.
- * @return {Promise} This returns a promise that yields the
- *      (future) fetched object. 
- */
+
 export function fetchItem(url) {
 	return axios.get(url)
 	.then(function(res) {
